58aff15e3193a917e8f69c535e237cdcc96b6fd0
olegush <43958767+olegush@users.noreply.github.com>
2018-10-24 13:38:28 +0300

    used collections.defaultdict

--- duplicates.py
+++ duplicates.py
@@ -1,27 +1,36 @@
 import sys
 import os
 from os.path import getsize
+from collections import defaultdict
 
 
 def get_duplicates(rootdirpath):
-    fileslist = []
-    paths = []
+    fileslist = defaultdict(dict)
     for path, dirs, files in os.walk(rootdirpath):
-        for file in files:
-            fileslist.append([file, str(getsize(path + '/' + file))])
-            paths.append(path)
-    for file, path in zip(fileslist, paths):
-        if fileslist.count(file) > 1:
-            print('{} ({} bytes) in {}'.format(file[0], file[1], path))
+        for filename in files:
+            filesize = str(getsize(os.path.join(path, filename)))
+            paths = [path]
+            if fileslist.get((filename, filesize)) is not None:
+                paths = fileslist.get((filename, filesize)) + [path]
+            fileslist[(filename, filesize)] = paths
+    filtered_dict = {
+        filenamesize: paths for filenamesize, paths
+        in fileslist.items() if len(paths) > 1
+    }
+    return filtered_dict
 
 
 if __name__ == '__main__':
     try:
         user_dirpath = sys.argv[1]
+        if os.path.isdir(user_dirpath):
+            print('Founded duplicates:')
+            duplicates_list = get_duplicates(user_dirpath).items()
+            for fileinfo, filepath in duplicates_list:
+                print('{} ({} bytes) in {}'
+                      .format(fileinfo[0], fileinfo[1], filepath)
+                      )
+        else:
+            print('No such directory')
     except IndexError:
         print('No script parameter (path to directory)')
-    except OSError:
-        print('No such directory')
-    else:
-        print('Founded duplicates:')
-        get_duplicates(user_dirpath)
